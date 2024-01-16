e507eb82d3c90598ea68bea21b9c344534ff4f14
demetsabuka <156785447+demetsabuka@users.noreply.github.com>
2024-01-16 18:45:58 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("FGUug6oFhks878dds4shGbSxhdv36ZPGcZgYu1u6AVHQ");
 
 #[program]
 pub mod nft_lend_borrow {
